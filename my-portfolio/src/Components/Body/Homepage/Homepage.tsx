d8c313338f4a8c13cf0e1268c77380652a45b301
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2023-06-02 21:49:47 +0530

    added typewriter

--- my-portfolio/src/Components/Body/Homepage/Homepage.tsx
+++ my-portfolio/src/Components/Body/Homepage/Homepage.tsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import photo from '../../../Assets/Images/myCoverPhoto.png';
 import Typewriter from 'typewriter-effect';
+import {Button, Col, Row} from "react-bootstrap";
+import * as Icon from "react-feather";
 
 const Homepage:React.FC = () => {
+
+
     return(
         <div className="home-page">
             <div className="photo-container">
                 <img src={photo} alt="My Photo" className="photo" />
             </div>
-            <Typewriter
-                onInit={(typewriter) => {
-                    typewriter
-                        .typeString('First line of text')
-                        .pauseFor(2000)
-                        .deleteAll()
-                        .typeString('Second line of text')
-                        .start();
-                }}
-            />
+
+            <div className="text-container">
+                <h1 className="home-header-name">Jeewantha Lahiru</h1>
+                <div className="typewriter-text">
+
+                    <Typewriter
+                        options={{
+                            strings: ['I\'m Full Stack Developer'],
+                            autoStart: true,
+                            loop: true,
+                            wrapperClassName: 'line-1',
+                            cursorClassName: 'custom-cursor',
+                            cursor: '|',
+                        }}
+                    />
+                </div>
+                <br/>
+                <div className="button-container">
+                    <Button className="icon-button">
+                        <Icon.Twitter/>
+                    </Button>
+                    <Button className="icon-button">
+                        <Icon.Facebook/>
+                    </Button>
+                    <Button className="icon-button">
+                        <Icon.Instagram/>
+                    </Button>
+                    <Button className="icon-button">
+                        <Icon.Linkedin/>
+                    </Button>
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
--- my-portfolio/src/Components/Navbar/NavigationBar.tsx
+++ my-portfolio/src/Components/Navbar/NavigationBar.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Button} from "react-bootstrap";
 import * as Icon from 'react-feather';
 
 const NavigationBar:React.FC = () => {
+
     return(
         <div className="Navbar">
             <div className="circle-buttons">
-                <Button variant="light" className="circle-button"> <Icon.Home/> </Button>
-                <Button variant="light" className="circle-button"><Icon.User/></Button>
-                <Button variant="light" className="circle-button"><Icon.File/></Button>
-                <Button variant="light" className="circle-button"><Icon.Briefcase/></Button>
-                <Button variant="light" className="circle-button"><Icon.Sliders/></Button>
-                <Button variant="light" className="circle-button"><Icon.Mail/></Button>
+                <Button variant="light" className="circle-button"> <Icon.Home/>
+                    <span className="button-text">Home</span>
+                </Button>
+                <Button variant="light" className="circle-button"><Icon.User/>
+                    <span className="button-text">About</span>
+                </Button>
+                <Button variant="light" className="circle-button"><Icon.File/>
+                    <span className="button-text">Resume</span>
+                </Button>
+                <Button variant="light" className="circle-button"><Icon.Briefcase/>
+                    <span className="button-text">Portfolio</span>
+                </Button>
+                <Button variant="light" className="circle-button"><Icon.Sliders/>
+                    <span className="button-text">Skills</span>
+                </Button>
+                <Button variant="light" className="circle-button"><Icon.Mail/>
+                    <span className="button-text">Contact</span>
+                </Button>
             </div>
         </div>
     )
